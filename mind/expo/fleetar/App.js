342435aa6292e6cfe29886f14bb9e4d2771ce555
Andrew Porter <mephisto83@hotmail.com>
2017-12-31 10:30:52 -0600

    update

---
+++ mind/expo/fleetar/App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import * as THREE from 'three';
+import ExpoTHREE from 'expo-three';
+import Expo from 'expo';
+console.disableYellowBox = true;
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Expo.GLView
+      ref={(ref) => this._glView = ref}
+        style={{ flex: 1 }}
+        onContextCreate={this._onGLContextCreate}
+      />
+    );
+  }
+
+  _onGLContextCreate = async (gl) => {
+    const arSession = await this._glView.startARSessionAsync();
+    // Do graphics stuff here!
+    const scene = new THREE.Scene();
+    // const camera = new THREE.PerspectiveCamera(
+    //   75, gl.drawingBufferWidth / gl.drawingBufferHeight, 0.1, 1000);
+
+    const camera = ExpoTHREE.createARCamera(
+      arSession,
+      gl.drawingBufferWidth,
+      gl.drawingBufferHeight,
+      0.01,
+      1000
+    );
+    const renderer = ExpoTHREE.createRenderer({ gl });
+    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+    const geometry = new THREE.BoxGeometry(0.07, 0.07, 0.07);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    cube.position.z = -1.4;
+    // camera.position.z = 5;
+    scene.background = ExpoTHREE.createARBackgroundTexture(arSession, renderer);
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.07;
+      cube.rotation.y += 0.04;
+      renderer.render(scene, camera);
+      gl.endFrameEXP();
+    }
+    animate();
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
